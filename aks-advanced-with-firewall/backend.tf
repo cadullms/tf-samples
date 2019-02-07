d613ac6e97f50b07e4ad0e6221ca856186320f0f
Carsten Düllmann <cadull@microsoct.com>
2019-02-07 19:42:13 +0100

    completely removing backend for now

--- aks-advanced-with-firewall/backend.tf
+++ aks-advanced-with-firewall/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "azurerm" {
-    # You can either remove this backend (and use local state) or need to have storage 
-    # account ready for remote state and specify storage_account_name, container_name, key 
-    # and a secret for this using partial configuration.
-    # See:
-    # https://www.terraform.io/docs/backends/types/azurerm.html
-    # https://www.terraform.io/docs/backends/config.html#partial-configuration
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     # You can either remove this backend (and use local state) or need to have storage 
+#     # account ready for remote state and specify storage_account_name, container_name, key 
+#     # and a secret for this using partial configuration.
+#     # See:
+#     # https://www.terraform.io/docs/backends/types/azurerm.html
+#     # https://www.terraform.io/docs/backends/config.html#partial-configuration
+#   }
+# }

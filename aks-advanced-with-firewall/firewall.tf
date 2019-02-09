3b14a5612abe57fad002778c862e24b6f8553134
Carsten Düllmann <cadull@microsoct.com>
2019-02-09 22:09:15 +0100

    .

--- aks-advanced-with-firewall/firewall.tf
+++ aks-advanced-with-firewall/firewall.tf
@@ -21,7 +21,7 @@ resource "azurerm_firewall_network_rule_collection" "aksnetwork" {
     name = "allow network"
 
     source_addresses = [
-      "10.0.0.0/16",
+      "*",
     ]
 
     destination_ports = [
@@ -30,7 +30,7 @@ resource "azurerm_firewall_network_rule_collection" "aksnetwork" {
     ]
 
     destination_addresses = [
-      "10.0.0.0/16",
+      "*",
     ]
 
     protocols = [

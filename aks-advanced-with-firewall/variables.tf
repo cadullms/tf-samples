5e6baa461eeb218bc0949563150f96dc2be0f8ee
Carsten Düllmann <cadull@microsoct.com>
2019-02-15 20:58:15 +0100

    further clarifying parameter descriptions

--- aks-advanced-with-firewall/variables.tf
+++ aks-advanced-with-firewall/variables.tf
@@ -23,13 +23,15 @@ variable "ssh_public_key_data" {
 }
 
 variable "sp_object_id" {
-  description="Not the app registration's object id needed here. It MUST be the (corresponding) service principal's object id. You can get that with: az ad sp list --show-mine --query \"[?displayName=='myapp'].objectId\""
+  description="Not the app registration's object id needed here. It MUST be the (corresponding) service principal's object id. You can get that with: az ad sp list --show-mine --query \"[?appId=='<sp_client_id>'].objectId\" where <sp_client_id> is the same value you specify here for the sp_client_id parameter. If that gives you no result, replace --show-mine with --all but be prepared for a longer wait..."
 }
 
 variable "sp_client_id" {
+  description="The client id (or app id) of the service principal that your AKS cluster will use to authenticate against Azure."
 }
 
 variable "sp_client_secret" {
+  description="The client secret of the service principal that your AKS cluster will use to authenticate against Azure."
 }
 
 variable "environment_name" {

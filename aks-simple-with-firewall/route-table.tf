b6343d9b69514a2b1e0c882f4f1266d233e98ca3
Carsten Düllmann <cadull@microsoct.com>
2019-02-15 22:05:13 +0100

    .

--- aks-simple-with-firewall/route-table.tf
+++ aks-simple-with-firewall/route-table.tf
@@ -8,7 +8,7 @@ data "external" "aks_route_table" {
 }
 
 data "azurerm_route_table" "aks_firewall_routes" {
-  name = "${data.external.aks_route_table.name}"
+  name = "${data.external.aks_route_table.result.route_table_name}"
   resource_group_name = "${azurerm_kubernetes_cluster.aks_cluster.node_resource_group}"
 }
 

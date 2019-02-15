bedc4844727014afd968c074f5418a21ea563796
Carsten Düllmann <cadull@microsoct.com>
2019-02-15 22:36:20 +0100

    .

--- aks-simple-with-firewall/route-table-get-name.sh
+++ aks-simple-with-firewall/route-table-get-name.sh
@@ -12,7 +12,8 @@ eval "$(jq -r '@sh "RESOURCE_GROUP=\(.resource_group)"')"
 # $ARM_CLIENT_SECRET
 # $ARM_TENANT_ID
 
-az aks login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
+# TODO: Do login in case we are not logged in yet.
+# az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
 $name = $(az network route-table list -g $RESOURCE_GROUP --query "[].name" -o tsv)
 
 jq -n --arg foobaz "$name" '{"name":$name}'

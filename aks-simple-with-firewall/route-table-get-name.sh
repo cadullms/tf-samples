d4f7deea54345bf8dbcc2f4004dbc43657d7b5cf
Carsten Düllmann <cadull@microsoct.com>
2019-02-16 10:03:37 +0100

    .

--- aks-simple-with-firewall/route-table-get-name.sh
+++ aks-simple-with-firewall/route-table-get-name.sh
@@ -7,13 +7,19 @@ set -e
 
 eval "$(jq -r '@sh "RESOURCE_GROUP=\(.resource_group)"')"
 
-# We assume that az is installed and these envs are set:
+# We assume that az is installed and either logged in or these envs are set:
 # $ARM_CLIENT_ID
 # $ARM_CLIENT_SECRET
 # $ARM_TENANT_ID
 
 # TODO: Do login in case we are not logged in yet.
 # az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
+
 name=$(az network route-table list -g $RESOURCE_GROUP --query "[].name" -o tsv)
+
+echo "ARM_CLIENT_ID: $ARM_CLIENT_ID"
+echo "ARM_TENANT_ID: $ARM_TENANT_ID"
+echo "RESOURCE_GROUP: $RESOURCE_GROUP"
+echo "name: $name"
 
 jq -n --arg name "$name" '{"name":$name}'

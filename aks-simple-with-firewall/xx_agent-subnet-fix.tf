0cd7f118543fb67ffb990781693373321f9c56e1
Carsten Düllmann <cadull@microsoct.com>
2019-02-23 01:28:05 +0100

    .

--- aks-simple-with-firewall/xx_agent-subnet-fix.tf
+++ aks-simple-with-firewall/xx_agent-subnet-fix.tf
@@ -38,8 +38,8 @@ resource "azurerm_subnet" "aks_agent" {
   resource_group_name       = "${azurerm_resource_group.aks_rg.name}"
   virtual_network_name      = "${azurerm_virtual_network.aks_vnet.name}"
   address_prefix            = "10.0.5.0/24"
-  #network_security_group_id = "${lookup(data.external.aks_node_resource_group_info_empty.result,"nsg_id")}"  
-  #route_table_id            = "${lookup(data.external.aks_node_resource_group_info_empty.result,"rt_id")}"  
+  network_security_group_id = "${lookup(data.external.aks_node_resource_group_info_empty.result,"nsg_id")}"  
+  route_table_id            = "${lookup(data.external.aks_node_resource_group_info_empty.result,"rt_id")}"  
   service_endpoints         = ["Microsoft.Sql","Microsoft.AzureCosmosDB","Microsoft.KeyVault","Microsoft.Storage"]
 }
 

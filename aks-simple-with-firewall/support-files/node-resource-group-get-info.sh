348638a3c4baa7a23e5008369859c96b7f0c5cf0
Carsten Düllmann <cadull@microsoct.com>
2019-02-23 01:09:07 +0100

    .

--- aks-simple-with-firewall/support-files/node-resource-group-get-info.sh
+++ aks-simple-with-firewall/support-files/node-resource-group-get-info.sh
@@ -1,5 +1,3 @@
-# This is only needed because we are setting the 
-
 #!/bin/bash
 
 # See https://www.terraform.io/docs/providers/external/data_source.html
@@ -24,6 +22,7 @@ echo "AKS_NAME          : $AKS_NAME" >> $logfile
 # az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
 
 node_rg_name=$(az aks list -g $AKS_RESOURCE_GROUP --query "[?name=='$AKS_NAME'].nodeResourceGroup" --o tsv)
+echo "node_rg_name : $node_rg_name" >> $logfile
 if [ -z "$node_rg_name" ]; then
     rt_name=""
     rt_id=""
@@ -37,10 +36,10 @@ else
     nsg_id=$(az network nsg list -g $node_rg_name --query "[].id" -o tsv)
 fi
 
-echo "rt_name  : $rt_name" >> $logfile
-echo "rt_id    : $rt_id" >> $logfile
-echo "nsg_name : $nsg_name" >> $logfile
-echo "nsg_id   : $nsg_id" >> $logfile
+echo "rt_name      : $rt_name" >> $logfile
+echo "rt_id        : $rt_id" >> $logfile
+echo "nsg_name     : $nsg_name" >> $logfile
+echo "nsg_id       : $nsg_id" >> $logfile
 
 jq -n \
   --arg rt_name "$rt_name"\

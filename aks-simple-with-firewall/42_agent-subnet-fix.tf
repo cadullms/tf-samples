028b684e17279363c7473f1dcaa57a8eb5975303
Carsten Düllmann <cadull@microsoct.com>
2019-02-23 02:12:57 +0100

    improving comments...

--- aks-simple-with-firewall/42_agent-subnet-fix.tf
+++ aks-simple-with-firewall/42_agent-subnet-fix.tf
@@ -1,14 +1,15 @@
 # See hint in https://www.terraform.io/docs/providers/azurerm/r/subnet_route_table_association.html
-# We currently need to do both: Set the id property in the subnet and do this
-# resource. Otherwise we will get problems on applying this a second time.
-# Terraform will then try to set route_table_id and nsg_id from the correct value to "",
-# because we did not specify them.
+# We currently need to do both: Set the id property in the subnet and do the
+# subnet_route_table_association# resource. If we do not set the id property,
+# we will get problems on applying this a second time. Terraform will then try 
+# to set route_table_id and nsg_id from the correct value to "", because we did not specify them.
+#
 # Yet that is causing circular dependencies in our configuration:
 # * For the subnet to be able to set the id, the route-table must exist
 # * The route-table in simple networking model is created during cluster creation
 # * Thus the cluster would need to be created before the subnet
 # * Yet for cluster creation the subnet already needs to be in place 
-# The last two requirements are a contradiction, so what can we do?
+# The last two requirements are a contradiction, so what can we do? See below...
 
 data "external" "aks_node_resource_group_info" {
   program = ["bash", "${path.module}/support-files/node-resource-group-get-info.sh"]
@@ -38,16 +39,17 @@ resource "azurerm_subnet" "aks_agent" {
   resource_group_name       = "${azurerm_resource_group.aks_rg.name}"
   virtual_network_name      = "${azurerm_virtual_network.aks_vnet.name}"
   address_prefix            = "10.0.5.0/24"
+  # See: This one is referencing the potentially empty early evaluated version of our external resource:
   network_security_group_id = "${lookup(data.external.aks_node_resource_group_info_empty.result,"nsg_id")}"  
   route_table_id            = "${lookup(data.external.aks_node_resource_group_info_empty.result,"rt_id")}"  
   service_endpoints         = ["Microsoft.Sql","Microsoft.AzureCosmosDB","Microsoft.KeyVault","Microsoft.Storage"]
 }
 
+# See: These are referencing the version of our external resource that is evaluatued after cluster creation:
 resource "azurerm_subnet_route_table_association" "aks_agent_subnet_to_rt" {
   subnet_id      = "${azurerm_subnet.aks_agent.id}"
   route_table_id = "${lookup(data.external.aks_node_resource_group_info.result,"rt_id")}"
 }
-
 resource "azurerm_subnet_network_security_group_association" "aks_agent_subnet_to_nsg" {
   subnet_id      = "${azurerm_subnet.aks_agent.id}"
   network_security_group_id = "${lookup(data.external.aks_node_resource_group_info.result,"nsg_id")}"

8f4286f221da208eb353c0908defc8ea6e44fe04
cadullms <cadullms@github.com>
2019-06-07 03:18:25 +0200

    last try for today

--- vm-windows-cse/main.tf
+++ vm-windows-cse/main.tf
@@ -160,8 +160,7 @@ resource "azurerm_virtual_machine_extension" "myvmext" {
   # C:\AzureData\CustomData.bin is the path where the custom_data passed to the os_profile (see above) lands
   settings = <<SETTINGS
         {   
-          "commandToExecute": "powershell -command copy-item \"c:\\AzureData\\CustomData.bin\" \"c:\\AzureData\\CustomData.ps1\";\"c:\\AzureData\\CustomData.ps1\""
-        }
+"powershell -command install-windowsfeature web-server;copy-item \"c:\\AzureData\\CustomData.bin\" \"c:\\AzureData\\CustomData.ps1\";\"c:\\AzureData\\CustomData.ps1\""        }
   SETTINGS
     
 }

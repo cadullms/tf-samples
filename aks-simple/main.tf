e2b0051eafb8d17ee65880cbaa7cdc5116eb54a7
cadullms <cadullms@github.com>
2020-03-30 00:32:19 +0200

    removed deprecated interpolation syntax

--- aks-simple/main.tf
+++ aks-simple/main.tf
@@ -1,36 +1,36 @@
 resource "azurerm_resource_group" "aks_rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "aks_simple" {
-  name                = "${var.cluster_name}"
-  location            = "${azurerm_resource_group.aks_rg.location}"
-  resource_group_name = "${azurerm_resource_group.aks_rg.name}"
-  dns_prefix          = "${var.dns_prefix}"
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.dns_prefix
 
   linux_profile {
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = "${var.ssh_public_key_data}"
+      key_data = var.ssh_public_key_data
     }
   }
 
   agent_pool_profile {
     name            = "default"
-    count           = "${var.agent_count}"
+    count           = var.agent_count
     vm_size         = "Standard_DS2_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
 
   service_principal {
-    client_id     = "${var.sp_client_id}"
-    client_secret = "${var.sp_client_secret}"
+    client_id     = var.sp_client_id
+    client_secret = var.sp_client_secret
   }
 
   tags {
-    Environment = "${var.environment_name}"
+    Environment = var.environment_name
   }
 }
